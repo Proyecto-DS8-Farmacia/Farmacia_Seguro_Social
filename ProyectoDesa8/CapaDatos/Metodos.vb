37a53e1c2bcecff4a1614ae2e67f2d237620e3bd
guillermo-gordon18-2000 <guillermo18zgordon@gmail.com>
2022-11-18 22:17:01 -0500

    "Actualizacion Perfil"

--- ProyectoDesa8/CapaDatos/Metodos.vb
+++ ProyectoDesa8/CapaDatos/Metodos.vb
@@ -31,4 +31,86 @@ Public Class Metodos
     End Function
 
 
+
+
+
+
+
+
+    Public Shared Function DATOSPACIENTEPORNOMBRE() As DataTable
+
+        Using CN As New SqlConnection(My.Settings.Conexion)
+            Using DA As New SqlDataAdapter("Pa_DatosDePacientePorNombre", CN)
+                Using TABLA As New DataTable
+                    DA.Fill(TABLA)
+                    Return TABLA
+                End Using
+            End Using
+        End Using
+    End Function
+
+    Public Shared Function DATOSDEUSUARIO() As DataTable
+
+        Using CN As New SqlConnection(My.Settings.Conexion)
+            Using DA As New SqlDataAdapter("Pa_ListarDatosDeUsuario", CN)
+                Using Data As New DataTable
+                    DA.Fill(Data)
+                    Return Data
+                End Using
+            End Using
+        End Using
+    End Function
+
+
+    Public Shared Function ActualizarUSUARIO(cedula As String, nombre As String, apellido As String, correo As String, telefono As String, farmacia As String) As String
+        Using CN As New SqlConnection(My.Settings.Conexion)
+            Using CMD As New SqlCommand("Pa_Actualizar", CN)
+                CMD.CommandType = CommandType.StoredProcedure
+                'CMD.Parameters.AddWithValue("@ENSQLSERVER", ENVISUALSTUDIO)
+                CMD.Parameters.AddWithValue("@Cedula", cedula)
+                CMD.Parameters.AddWithValue("@nombre", nombre)
+                CMD.Parameters.AddWithValue("@apellido", apellido)
+                CMD.Parameters.AddWithValue("@telefono", telefono)
+                CMD.Parameters.AddWithValue("@correo", correo)
+                CMD.Parameters.AddWithValue("@farma", farmacia)
+                CN.Open()
+                Return CMD.ExecuteReader().HasRows.ToString
+
+            End Using
+        End Using
+
+
+    End Function
+
+
+
+    Public Shared Function FARMACIAS() As DataTable
+
+        Using CN As New SqlConnection(My.Settings.Conexion)
+            Using DA As New SqlDataAdapter("select nombre from farmacia", CN)
+                Using Data As New DataTable
+                    DA.Fill(Data)
+
+                    Return Data
+                End Using
+            End Using
+        End Using
+    End Function
+
+    Public Shared Function TIPOSDEUSUARIO() As DataTable
+
+        Using CN As New SqlConnection(My.Settings.Conexion)
+            Using DA As New SqlDataAdapter("select DISTINCT tipo from Usuario ", CN)
+                Using Data As New DataTable
+                    DA.Fill(Data)
+                    Return Data
+                End Using
+            End Using
+        End Using
+    End Function
+
+
+
+
+
 End Class
